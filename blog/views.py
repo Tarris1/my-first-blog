6beb2c34ce97295d81e7daa1552b8571558ca239
Tarris1 <tarris1@hotmail.com>
2019-07-23 10:03:57 +0200

    Added view and and template for detailed blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post #. in same directory
 # Create your views here.
@@ -8,5 +8,7 @@ def post_list(request):
 	return render(request,'blog/post_list.html',{'posts': posts}) #Render puts together the template blog/post_list.html
 	#Request -> everything received from user via internet
 
-
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk = pk)
+	return render (request, 'blog/post_detail.html', {'post':post})
 
